7a7b8ccdc8a5c74b24b3709ed48fce4fb871f0ac
Ronan Collobert <collobert@apple.com>
2024-01-11 16:34:34 -0800

    mlx_jvp/vjp support

--- mlx/c/transforms.cpp
+++ mlx/c/transforms.cpp
@@ -1,12 +1,35 @@
 #include "mlx/c/transforms.h"
 
 #include "mlx/c/private/array.h"
+#include "mlx/c/private/closure.h"
 #include "mlx/c/private/stream.h"
 #include "mlx/c/private/utils.h"
 
 extern "C" void mlx_eval(const mlx_array* outputs, size_t num_outputs) {
   return MLX_C_VOID(mlx::core::eval(MLX_CPP_ARRVEC(outputs, num_outputs)));
 }
+extern "C" mlx_vector_vector_array mlx_jvp(
+    mlx_closure fun,
+    const mlx_array* primals,
+    size_t num_primals,
+    const mlx_array* tangents,
+    size_t num_tangents) {
+  return MLX_C_VECTORARRAYPAIR(mlx::core::jvp(
+      MLX_CPP_CLOSURE(fun),
+      MLX_CPP_ARRVEC(primals, num_primals),
+      MLX_CPP_ARRVEC(tangents, num_tangents)));
+}
 extern "C" void mlx_simplify(const mlx_array* outputs, size_t num_outputs) {
   return MLX_C_VOID(mlx::core::simplify(MLX_CPP_ARRVEC(outputs, num_outputs)));
+}
+extern "C" mlx_vector_vector_array mlx_vjp(
+    mlx_closure fun,
+    const mlx_array* primals,
+    size_t num_primals,
+    const mlx_array* cotangents,
+    size_t num_cotangents) {
+  return MLX_C_VECTORARRAYPAIR(mlx::core::vjp(
+      MLX_CPP_CLOSURE(fun),
+      MLX_CPP_ARRVEC(primals, num_primals),
+      MLX_CPP_ARRVEC(cotangents, num_cotangents)));
 }
--- mlx/c/transforms.h
+++ mlx/c/transforms.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include "mlx/c/array.h"
+#include "mlx/c/closure.h"
 #include "mlx/c/stream.h"
 
 #ifdef __cplusplus
@@ -11,7 +12,19 @@ extern "C" {
 #endif
 
 void mlx_eval(const mlx_array* outputs, size_t num_outputs);
+mlx_vector_vector_array mlx_jvp(
+    mlx_closure fun,
+    const mlx_array* primals,
+    size_t num_primals,
+    const mlx_array* tangents,
+    size_t num_tangents);
 void mlx_simplify(const mlx_array* outputs, size_t num_outputs);
+mlx_vector_vector_array mlx_vjp(
+    mlx_closure fun,
+    const mlx_array* primals,
+    size_t num_primals,
+    const mlx_array* cotangents,
+    size_t num_cotangents);
 
 #ifdef __cplusplus
 }
