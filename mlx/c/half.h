01efac90ba1d6856f177191157eb6597a2a9c069
David Koski <dkoski@apple.com>
2024-02-03 13:27:33 -0800

    Allow half float on aarch64 -- per clang this is supported but oddly __ARM_FEATURE_FP16_SCALAR_ARITHMETIC is not defined in all cases (e.g. iOS)

--- mlx/c/half.h
+++ mlx/c/half.h
@@ -7,13 +7,13 @@
 extern "C" {
 #endif
 
-#ifdef __ARM_FEATURE_FP16_SCALAR_ARITHMETIC
+#if defined(__ARM_FEATURE_FP16_SCALAR_ARITHMETIC) || defined(__aarch64__)
 #define HAS_FLOAT16
 #include <arm_fp16.h>
 typedef __fp16 float16_t;
 #endif
 
-#ifdef __ARM_FEATURE_BF16
+#if defined(__ARM_FEATURE_BF16) || defined(__aarch64__)
 #define HAS_BFLOAT16
 #include <arm_bf16.h>
 typedef __bf16 bfloat16_t;
