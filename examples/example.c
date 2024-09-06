2027d83572b8ff8fa9ea802b4d9c1eaa8462b89f
Ronan Collobert <collobert@apple.com>
2024-09-06 16:12:05 -0700

    show mlx_metal_device_info() in example

--- examples/example.c
+++ examples/example.c
@@ -10,7 +10,29 @@ void print_array(const char* msg, mlx_array arr) {
   mlx_free(str);
 }
 
+void gpu_info() {
+  printf("==================================================\n");
+  printf("GPU info:\n");
+  mlx_map_string_to_string_size_t_variant info = mlx_metal_device_info();
+  mlx_map_string_to_string_size_t_variant_iterator it =
+      mlx_map_string_to_string_size_t_variant_iterate(info);
+  while (mlx_map_string_to_string_size_t_variant_iterator_next(it)) {
+    mlx_string key = mlx_map_string_to_string_size_t_variant_iterator_key(it);
+    mlx_string_size_t_variant val =
+        mlx_map_string_to_string_size_t_variant_iterator_value(it);
+    mlx_string val_str = mlx_tostring(val);
+    printf("  %s: %s\n", mlx_string_data(key), mlx_string_data(val_str));
+    mlx_free(key);
+    mlx_free(val);
+    mlx_free(val_str);
+  }
+  mlx_free(it);
+  mlx_free(info);
+  printf("==================================================\n");
+}
 int main() {
+  gpu_info();
+
   mlx_stream stream = mlx_gpu_stream();
   float data[] = {1, 2, 3, 4, 5, 6};
   int shape[] = {2, 3};
