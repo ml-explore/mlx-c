67af3f024dd4a32bb48c3092bff6ecc384273564
Ronan Collobert <collobert@apple.com>
2024-09-23 16:46:33 -0700

    vector gen hot fix

--- mlx/c/vector.h
+++ mlx/c/vector.h
@@ -37,8 +37,7 @@ void mlx_vector_array_add_data(
     size_t size);
 void mlx_vector_array_add_value(mlx_vector_array vec, const mlx_array val);
 size_t mlx_vector_array_size(mlx_vector_array vec);
-mlx_array mlx_vector_array_get(mlx_vector_array vec, size_t idx);
-mlx_array* mlx_vector_array_data(mlx_vector_array vec);
+int mlx_vector_array_get(const mlx_vector_array vec, size_t idx, mlx_array);
 
 /**
  * A vector of vector_array.
@@ -65,10 +64,10 @@ void mlx_vector_vector_array_add_value(
     mlx_vector_vector_array vec,
     const mlx_vector_array val);
 size_t mlx_vector_vector_array_size(mlx_vector_vector_array vec);
-mlx_vector_array mlx_vector_vector_array_get(
-    mlx_vector_vector_array vec,
-    size_t idx);
-mlx_vector_array* mlx_vector_vector_array_data(mlx_vector_vector_array vec);
+int mlx_vector_vector_array_get(
+    const mlx_vector_vector_array vec,
+    size_t idx,
+    mlx_vector_array);
 
 /**
  * A vector of int.
@@ -82,8 +81,7 @@ int mlx_vector_int_set_value(mlx_vector_int vec, const int val);
 void mlx_vector_int_add_data(mlx_vector_int vec, const int* data, size_t size);
 void mlx_vector_int_add_value(mlx_vector_int vec, const int val);
 size_t mlx_vector_int_size(mlx_vector_int vec);
-int mlx_vector_int_get(mlx_vector_int vec, size_t idx);
-int* mlx_vector_int_data(mlx_vector_int vec);
+int mlx_vector_int_get(const mlx_vector_int vec, size_t idx, int*);
 
 /**
  * A vector of vector_int.
@@ -110,8 +108,10 @@ void mlx_vector_vector_int_add_value(
     mlx_vector_vector_int vec,
     const mlx_vector_int val);
 size_t mlx_vector_vector_int_size(mlx_vector_vector_int vec);
-mlx_vector_int mlx_vector_vector_int_get(mlx_vector_vector_int vec, size_t idx);
-mlx_vector_int* mlx_vector_vector_int_data(mlx_vector_vector_int vec);
+int mlx_vector_vector_int_get(
+    const mlx_vector_vector_int vec,
+    size_t idx,
+    mlx_vector_int);
 
 /**
  * A vector of string.
@@ -133,8 +133,7 @@ void mlx_vector_string_add_data(
     size_t size);
 void mlx_vector_string_add_value(mlx_vector_string vec, const mlx_string val);
 size_t mlx_vector_string_size(mlx_vector_string vec);
-mlx_string mlx_vector_string_get(mlx_vector_string vec, size_t idx);
-mlx_string* mlx_vector_string_data(mlx_vector_string vec);
+int mlx_vector_string_get(const mlx_vector_string vec, size_t idx, mlx_string);
 
 /**@}*/
 
