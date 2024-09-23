04373348d0558a0ddbfeb3c57bfd2e92c56d3fc6
Ronan Collobert <collobert@apple.com>
2024-09-23 16:52:54 -0700

    support for mlx_closure_metal_kernel

--- mlx/c/closure.h
+++ mlx/c/closure.h
@@ -129,6 +129,51 @@ int mlx_closure_custom_vmap_apply(
     mlx_vector_array res_0,
     mlx_vector_int res_1);
 
+typedef struct mlx_closure_metal_kernel_desc_* mlx_closure_metal_kernel_desc;
+mlx_closure_metal_kernel_desc mlx_closure_metal_kernel_desc_new();
+int mlx_closure_metal_kernel_desc_add_output_arg(
+    mlx_closure_metal_kernel_desc desc,
+    const int* shape,
+    size_t size,
+    mlx_array_dtype dtype);
+int mlx_closure_metal_kernel_desc_set_grid(
+    mlx_closure_metal_kernel_desc desc,
+    int grid1,
+    int grid2,
+    int grid3);
+int mlx_closure_metal_kernel_desc_set_thread_group(
+    mlx_closure_metal_kernel_desc desc,
+    int thread1,
+    int thread2,
+    int thread3);
+int mlx_closure_metal_kernel_desc_set_init_value(
+    mlx_closure_metal_kernel_desc desc,
+    float value);
+int mlx_closure_metal_kernel_desc_set_verbose(
+    mlx_closure_metal_kernel_desc desc,
+    bool verbose);
+int mlx_closure_metal_kernel_desc_add_template_arg_dtype(
+    mlx_closure_metal_kernel_desc desc,
+    const char* name,
+    mlx_array_dtype dtype);
+int mlx_closure_metal_kernel_desc_add_template_arg_int(
+    mlx_closure_metal_kernel_desc desc,
+    const char* name,
+    int value);
+int mlx_closure_metal_kernel_desc_add_template_arg_bool(
+    mlx_closure_metal_kernel_desc desc,
+    const char* name,
+    bool value);
+
+typedef struct mlx_closure_metal_kernel_* mlx_closure_metal_kernel;
+mlx_closure_metal_kernel mlx_closure_metal_kernel_new();
+int mlx_closure_metal_kernel_apply(
+    mlx_closure_metal_kernel cls,
+    const mlx_vector_array inputs,
+    const mlx_closure_metal_kernel_desc desc,
+    const mlx_stream stream,
+    mlx_vector_array outputs);
+
 /**@}*/
 
 #ifdef __cplusplus
