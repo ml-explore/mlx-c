17431c88352777f100ae76438b0adb157e766681
Ronan Collobert <collobert@apple.com>
2024-10-02 16:22:33 -0700

    version 0.0.10

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -12,7 +12,7 @@ import mlx.core as mx
 project = "MLX C"
 copyright = "2023-2024, MLX Contributors"
 author = "MLX Contributors"
-version = "0.0.9"
+version = "0.0.10"
 release = version
 
 # -- General configuration ---------------------------------------------------
