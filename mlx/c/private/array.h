7211fedaa500f194d1e0e6edf2040a41c6fb34d7
Ronan Collobert <collobert@apple.com>
2024-10-03 10:58:25 -0700

    static empty array

--- mlx/c/private/array.h
+++ mlx/c/private/array.h
@@ -7,10 +7,11 @@
 #include "mlx/c/private/object.h"
 #include "mlx/mlx.h"
 
+static mlx::core::array
+    mlx_empty_array_(std::vector<int>{0}, mlx::core::float32, nullptr, {});
+
 struct mlx_array_ : mlx_object_ {
-  mlx_array_()
-      : mlx_object_(),
-        ctx(std::vector<int>{0}, mlx::core::float32, nullptr, {}){};
+  mlx_array_() : mlx_object_(), ctx(mlx_empty_array_){};
   mlx_array_(mlx::core::array ctx) : mlx_object_(), ctx(ctx){};
   virtual mlx_string_* tostring() override;
   mlx::core::array ctx;
