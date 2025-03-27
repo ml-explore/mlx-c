9dfdf86b9c87ec2f999a6964b9104587d63cfaed
Ronan Collobert <collobert@apple.com>
2025-03-27 10:11:17 -0700

    Explicit float64 scalar array constructors now return actually float64 arrays (#60)
    * explicit float64 scalar array constructors now returns actual float64 arrays
    
    * example-float64: show updated float64 semantic

--- examples/example-float64.c
+++ examples/example-float64.c
@@ -15,11 +15,16 @@ int main() {
   double data[] = {1, 2, 3, 4, 5, 6};
   int shape[] = {2, 3};
   mlx_array arr = mlx_array_new_data(data, shape, 2, MLX_FLOAT64);
-  print_array("hello world!", arr);
+  print_array("hello world in float64!", arr);
+
+  mlx_array three = mlx_array_new_float64(3);
+  print_array("a float64 scalar array", three);
+  mlx_multiply(&arr, arr, three, stream);
+  print_array("multiply previous array by 3!", arr);
 
   mlx_array two = mlx_array_new_int(2);
   mlx_divide(&arr, arr, two, stream);
-  print_array("divive by 2!", arr);
+  print_array("divive by 2 (integer)", arr);
 
   mlx_arange(&arr, 0, 3, 0.5, MLX_FLOAT64, stream);
   print_array("arange", arr);
