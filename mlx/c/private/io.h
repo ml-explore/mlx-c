423045b2c174ae7f611245e7928d9034542145d3
Ronan Collobert <collobert@apple.com>
2025-03-31 16:16:17 -0700

    iotypes: introducing mlx_io_reader and mlx_io_writer

--- mlx/c/private/io.h
+++ mlx/c/private/io.h
@@ -4,6 +4,111 @@
 #include <iostream>
 #include "mlx/mlx.h"
 
+class CReader : public mlx::core::io::Reader {
+ public:
+  void* desc;
+  mlx_io_vtable vtable;
+
+  CReader(void* desc, mlx_io_vtable vtable) : desc(desc), vtable(vtable) {};
+  virtual bool is_open() const override {
+    return vtable.is_open(desc);
+  };
+  virtual bool good() const override {
+    return vtable.good(desc);
+  };
+  virtual size_t tell() override {
+    return vtable.tell(desc);
+  }
+  virtual void seek(
+      int64_t off,
+      std::ios_base::seekdir way = std::ios_base::beg) override {
+    switch (way) {
+      case std::ios_base::beg:
+        return vtable.seek(desc, off, SEEK_SET);
+        break;
+      case std::ios_base::cur:
+        return vtable.seek(desc, off, SEEK_CUR);
+        break;
+      case std::ios_base::end:
+        return vtable.seek(desc, off, SEEK_END);
+        break;
+      default:
+        throw std::runtime_error("mlx_io_reader: invalid seek way");
+    }
+  }
+  virtual void read(char* data, size_t n) override {
+    return vtable.read(desc, data, n);
+  };
+  virtual void read(char* data, size_t n, size_t offset) override {
+    return vtable.read_at_offset(desc, data, n, offset);
+  };
+  virtual std::string label() const override {
+    return vtable.label(desc);
+  };
+};
+
+class CWriter : public mlx::core::io::Writer {
+ public:
+  void* desc;
+  mlx_io_vtable vtable;
+
+  CWriter(void* desc, mlx_io_vtable vtable) : desc(desc), vtable(vtable) {};
+  virtual bool is_open() const override {
+    return vtable.is_open(desc);
+  };
+  virtual bool good() const override {
+    return vtable.good(desc);
+  };
+  virtual size_t tell() override {
+    return vtable.tell(desc);
+  }
+  virtual void seek(
+      int64_t off,
+      std::ios_base::seekdir way = std::ios_base::beg) override {
+    switch (way) {
+      case std::ios_base::beg:
+        return vtable.seek(desc, off, SEEK_SET);
+        break;
+      case std::ios_base::cur:
+        return vtable.seek(desc, off, SEEK_CUR);
+        break;
+      case std::ios_base::end:
+        return vtable.seek(desc, off, SEEK_END);
+        break;
+      default:
+        throw std::runtime_error("mlx_io_writer: invalid seek way");
+    }
+  }
+  virtual void write(const char* data, size_t n) override {
+    return vtable.write(desc, data, n);
+  };
+  virtual std::string label() const override {
+    return vtable.label(desc);
+  };
+};
+
+struct creader_holder {
+  std::shared_ptr<CReader> ptr;
+};
+
+inline mlx_io_reader mlx_io_reader_new_(void* uctx, mlx_io_vtable vtable) {
+  return mlx_io_reader(
+      {new creader_holder({std::make_shared<CReader>(uctx, vtable)})});
+}
+
+inline std::shared_ptr<CReader> mlx_io_reader_get_(mlx_io_reader d) {
+  if (!d.ctx) {
+    throw std::runtime_error("expected a non-empty mlx_io_reader");
+  }
+  return static_cast<creader_holder*>(d.ctx)->ptr;
+}
+
+inline void mlx_io_reader_free_(mlx_io_reader io) {
+  if (io.ctx) {
+    delete static_cast<creader_holder*>(io.ctx);
+  }
+}
+
 class CFILEReader : public mlx::core::io::Reader {
  private:
   FILE* f;
@@ -47,6 +152,13 @@ class CFILEReader : public mlx::core::io::Reader {
     return "FILE (read mode)";
   };
 };
+
+inline std::shared_ptr<CWriter> mlx_io_writer_get_(mlx_io_writer d) {
+  if (!d.ctx) {
+    throw std::runtime_error("expected a non-empty mlx_io_writer");
+  }
+  return *static_cast<std::shared_ptr<CWriter>*>(d.ctx);
+}
 
 class CFILEWriter : public mlx::core::io::Writer {
  private:
