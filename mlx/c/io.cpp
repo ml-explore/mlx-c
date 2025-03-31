6d81997d2998f1d38fcf1b8d9031fefad2adaf19
Ronan Collobert <collobert@apple.com>
2025-03-31 16:19:45 -0700

    regenerate

--- mlx/c/io.cpp
+++ mlx/c/io.cpp
@@ -9,12 +9,11 @@
 #include "mlx/io.h"
 
 extern "C" int
-mlx_load_file(mlx_array* res, FILE* in_stream, const mlx_stream s) {
+mlx_load_reader(mlx_array* res, mlx_io_reader in_stream, const mlx_stream s) {
   try {
     mlx_array_set_(
         *res,
-        mlx::core::load(
-            std::make_shared<CFILEReader>(in_stream), mlx_stream_get_(s)));
+        mlx::core::load(mlx_io_reader_get_(in_stream), mlx_stream_get_(s)));
   } catch (std::exception& e) {
     mlx_error(e.what());
     return 1;
@@ -31,15 +30,15 @@ extern "C" int mlx_load(mlx_array* res, const char* file, const mlx_stream s) {
   }
   return 0;
 }
-extern "C" int mlx_load_safetensors_file(
+extern "C" int mlx_load_safetensors_reader(
     mlx_map_string_to_array* res_0,
     mlx_map_string_to_string* res_1,
-    FILE* in_stream,
+    mlx_io_reader in_stream,
     const mlx_stream s) {
   try {
     {
       auto [tpl_0, tpl_1] = mlx::core::load_safetensors(
-          std::make_shared<CFILEReader>(in_stream), mlx_stream_get_(s));
+          mlx_io_reader_get_(in_stream), mlx_stream_get_(s));
       mlx_map_string_to_array_set_(*res_0, tpl_0);
       mlx_map_string_to_string_set_(*res_1, tpl_1);
     };
@@ -67,10 +66,9 @@ extern "C" int mlx_load_safetensors(
   }
   return 0;
 }
-extern "C" int mlx_save_file(FILE* out_stream, const mlx_array a) {
+extern "C" int mlx_save_writer(mlx_io_writer out_stream, const mlx_array a) {
   try {
-    mlx::core::save(
-        std::make_shared<CFILEWriter>(out_stream), mlx_array_get_(a));
+    mlx::core::save(mlx_io_writer_get_(out_stream), mlx_array_get_(a));
   } catch (std::exception& e) {
     mlx_error(e.what());
     return 1;
@@ -86,13 +84,13 @@ extern "C" int mlx_save(const char* file, const mlx_array a) {
   }
   return 0;
 }
-extern "C" int mlx_save_safetensors_file(
-    FILE* in_stream,
+extern "C" int mlx_save_safetensors_writer(
+    mlx_io_writer in_stream,
     const mlx_map_string_to_array param,
     const mlx_map_string_to_string metadata) {
   try {
     mlx::core::save_safetensors(
-        std::make_shared<CFILEWriter>(in_stream),
+        mlx_io_writer_get_(in_stream),
         mlx_map_string_to_array_get_(param),
         mlx_map_string_to_string_get_(metadata));
   } catch (std::exception& e) {
