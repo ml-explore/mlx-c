1e3c24ffebfdfbeecca054c51637fc4381d98aab
Ronan Collobert <collobert@apple.com>
2026-04-07 16:56:39 -0700

    support for mlx_distributed_group_free() (#110)
    * support for mlx_distributed_group_free()
    
    * comply to the MLX C API

--- mlx/c/distributed_group.cpp
+++ mlx/c/distributed_group.cpp
@@ -6,6 +6,41 @@
 #include "mlx/c/error.h"
 #include "mlx/c/private/mlx.h"
 
+extern "C" mlx_distributed_group mlx_distributed_group_new(void) {
+  try {
+    return mlx_distributed_group_new_();
+  } catch (std::exception& e) {
+    mlx_error(e.what());
+    return {nullptr};
+  }
+}
+
+extern "C" int mlx_distributed_group_free(mlx_distributed_group group) {
+  try {
+    mlx_distributed_group_free_(group);
+  } catch (std::exception& e) {
+    mlx_error(e.what());
+    return 1;
+  }
+  return 0;
+}
+
+extern "C" int
+mlx_distributed_init(mlx_distributed_group* res, bool strict, const char* bk) {
+  try {
+    if (bk) {
+      mlx_distributed_group_set_(
+          *res, mlx::core::distributed::init(strict, bk));
+    } else {
+      mlx_distributed_group_set_(*res, mlx::core::distributed::init(strict));
+    }
+    return 0;
+  } catch (std::exception& e) {
+    mlx_error(e.what());
+    return 1;
+  }
+}
+
 extern "C" int mlx_distributed_group_rank(mlx_distributed_group group) {
   try {
     return mlx_distributed_group_get_(group).rank();
@@ -24,14 +59,18 @@ extern "C" int mlx_distributed_group_size(mlx_distributed_group group) {
   }
 }
 
-extern "C" mlx_distributed_group
-mlx_distributed_group_split(mlx_distributed_group group, int color, int key) {
+extern "C" int mlx_distributed_group_split(
+    mlx_distributed_group* res,
+    mlx_distributed_group group,
+    int color,
+    int key) {
   try {
-    return mlx_distributed_group_new_(
-        mlx_distributed_group_get_(group).split(color, key));
+    mlx_distributed_group_set_(
+        *res, mlx_distributed_group_get_(group).split(color, key));
+    return 0;
   } catch (std::exception& e) {
     mlx_error(e.what());
-    return mlx_distributed_group_new_();
+    return 1;
   }
 }
 
@@ -45,21 +84,5 @@ extern "C" bool mlx_distributed_is_available(const char* bk) {
   } catch (std::exception& e) {
     mlx_error(e.what());
     return false;
-  }
-}
-
-extern "C" mlx_distributed_group mlx_distributed_init(
-    bool strict,
-    const char* bk) {
-  try {
-    if (bk) {
-      return mlx_distributed_group_new_(
-          mlx::core::distributed::init(strict, bk));
-    } else {
-      return mlx_distributed_group_new_(mlx::core::distributed::init(strict));
-    }
-  } catch (std::exception& e) {
-    mlx_error(e.what());
-    return mlx_distributed_group_new_();
   }
 }
